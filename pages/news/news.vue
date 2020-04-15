94600d54057b1f9cdeb2f93ac1491f10dfc46c5a
mingWang <1603657728@qq.com>
2020-04-15 15:23:52 +0800

    3-11 uniApp生命周期

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,6 +1,32 @@
 <template>
 	<view>
-		news
+		<view v-show="false">v-show</view>
+    <view v-if="false">v-if</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view>
+    <view>xxx</view><view>xxx</view>
+    <view>xxx</view>
+    
 	</view>
 </template>
 
@@ -11,6 +37,46 @@
 				
 			}
 		},
+    onLoad() {
+      // 进入此页面初触发
+      console.log('页面初始化 执行一次 onload')
+    },
+    onReady() {
+      // 此页面加载完成的时候触发
+      console.log('页面加载完成 执行一次 onReady')
+    },
+    onShow() {
+      // 显示此页面的时候触发
+      console.log('页面进入执行 执行多次 onShow')
+    },
+    onHide() {
+      // 离开了此页面触发
+      console.log('页面进入离开 执行多次 onHide')
+    },
+    onPullDownRefresh() {
+      console.log('用户执行了下拉加载动作')
+    },
+    // 页面滚动到底部触发
+    onReachBottom() {
+      console.log('你已经到底了')
+    },
+    // 切换到该页面时触发
+    onTabItemTap() {
+      console.log('欢迎收看新闻')
+    },
+    // 用户触发分享
+    onShareAppMessage() {
+      console.log('用户进行了分享')
+      return {
+        title: "自定义的分享标题",
+        path: '/pages/news/news',
+        imageUrl: '/static/logo.png' 
+      }
+    },
+    // 参数是滚动条距离顶部的像素值
+    onPageScroll(obj) {
+      console.log(obj)
+    },
 		methods: {
 			
 		}
--- unpackage/dist/dev/mp-weixin/pages/news/news.js
+++ unpackage/dist/dev/mp-weixin/pages/news/news.js
@@ -135,12 +135,78 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 var _default =
 {
   data: function data() {
     return {};
 
 
+  },
+  onLoad: function onLoad() {
+    // 进入此页面初触发
+    console.log('页面初始化 执行一次 onload');
+  },
+  onReady: function onReady() {
+    // 此页面加载完成的时候触发
+    console.log('页面加载完成 执行一次 onReady');
+  },
+  onShow: function onShow() {
+    // 显示此页面的时候触发
+    console.log('页面进入执行 执行多次 onShow');
+  },
+  onHide: function onHide() {
+    // 离开了此页面触发
+    console.log('页面进入离开 执行多次 onHide');
+  },
+  onPullDownRefresh: function onPullDownRefresh() {
+    console.log('用户执行了下拉加载动作');
+  },
+  // 页面滚动到底部触发
+  onReachBottom: function onReachBottom() {
+    console.log('你已经到底了');
+  },
+  // 切换到该页面时触发
+  onTabItemTap: function onTabItemTap() {
+    console.log('欢迎收看新闻');
+  },
+  // 用户触发分享
+  onShareAppMessage: function onShareAppMessage() {
+    console.log('用户进行了分享');
+    return {
+      title: "自定义的分享标题",
+      path: '/pages/news/news',
+      imageUrl: '/static/logo.png' };
+
+  },
+  // 参数是滚动条距离顶部的像素值
+  onPageScroll: function onPageScroll(obj) {
+    console.log(obj);
   },
   methods: {} };exports.default = _default;
 
